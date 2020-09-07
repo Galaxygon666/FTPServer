0c7fb13a2e8370988c1893295fff728e1d8c7bec
Galaxygon666 <thepower2311@gmail.com>
2020-09-07 15:43:04 +0200

    Made a server folder

---
+++ Server/src/server.java
@@ -0,0 +1,14 @@
+import java.net.*;
+import java.io.*;
+
+public class server
+{
+    private Socket socket = null;
+    private ServerSocket serversock = null;
+    private DataInputStream input = null;
+
+    public void Server(int port)
+    {
+
+    }
+}
