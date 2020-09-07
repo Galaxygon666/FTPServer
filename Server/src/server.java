9c30d67f6c78a7309fcb0ba79e55651ea1d9ae61
Galaxygon666 <thepower2311@gmail.com>
2020-09-07 20:01:45 +0200

    Idk

--- Server/src/server.java
+++ Server/src/server.java
@@ -7,6 +7,11 @@ public class server
     private ServerSocket serversock = null;
     private DataInputStream input = null;
 
+    public static void main(String[] args)
+    {
+
+    }
+
     public void Server(int port)
     {
 
