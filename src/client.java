cd79f3ba920f48ebbaf6c5ae682c2a7ddca2e7fa
Galaxygon666 <thepower2311@gmail.com>
2020-09-07 15:37:32 +0200

    Made server and client class

---
+++ src/client.java
@@ -0,0 +1,15 @@
+import java.net.*;
+import java.io.*;
+
+public class client
+{
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream output = null;
+
+    public void Client(String adress, int port)
+    {
+
+    }
+
+}
