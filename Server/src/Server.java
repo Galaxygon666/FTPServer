be04d1f84fb56276917bd1d6a107768353a44741
Galaxygon666 <thepower2311@gmail.com>
2020-09-10 21:40:55 +0200

    Updated gui on both sides

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
 import java.net.*;
 import java.io.*;
+
 
 public class Server
 {
@@ -13,11 +16,11 @@ public class Server
     private DataInputStream input = null;
 
     static JFrame frame;
-    static JButton btn;
-    static JLabel label;
-    static JLabel start;
-    static JLabel wait;
-
+    static JPanel panel;
+    static JLabel title;
+    static JLabel status;
+    static JLabel ip;
+    static JButton conn;
 
     public Server(int port)
     {
@@ -29,10 +32,14 @@ public class Server
 
             socket = server.accept();
             System.out.println("Connection accepted!");
-            start.setText("Connection made!");
+            //System.out.println(socket.getInetAddress());
+
+            String ipClient = socket.getInetAddress().toString();
+            status.setText("Connection made!");
+            ip.setText("Connected to: " + ipClient.replace("/", ""));
+
 
             input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-
             String line = "";
 
             while (!line.equals("Over"))
@@ -48,7 +55,9 @@ public class Server
                     break;
                 }
             }
-            System.out.println("Connection closing");
+            System.out.println("Connection lost...");
+            status.setText("Connection lost...");
+            ip.setText("");
 
             socket.close();
             input.close();
@@ -64,39 +73,64 @@ public class Server
 
     public static void main(String args[])
     {
-        frame = new JFrame("Server client");
-        btn = new JButton("Start server");
+        frame = new JFrame("DragonFiles: Server side");
+        frame.setIconImage(Toolkit.getDefaultToolkit().getImage("./resources/Icon.png"));
+        panel = new JPanel();
+        panel.setBounds(0,0,400,400);
+        panel.setBackground(new Color(49, 49, 49));
+
+        // Input field Label
+        title = new JLabel("Start the server on port 5000");
+        title.setForeground(new Color(255, 255, 255));
+        title.setFont(new Font("Arial", Font.PLAIN, 16));
+        title.setBounds(90,70,200,30);
+
+        // Server connect button
+        conn = new JButton("Start");
+        conn.setBackground(new Color(202, 62, 71));
+        conn.setForeground(Color.white);
+        conn.setBounds(90,100,200,30);
+
+        // Connection made Lable
+        status = new JLabel("Waiting for connection...");
+        status.setBounds(90,135,200,30);
+        status.setFont(new Font("Arial", Font.PLAIN, 14));
+        status.setForeground(Color.white);
+
+        // Display client ip
+        ip = new JLabel();
+        ip.setBounds(90,160,200,30);
+        ip.setFont(new Font("Arial", Font.PLAIN, 14));
+        ip.setForeground(Color.white);
+        ip.setVisible(true);
 
 
-        label = new JLabel("Start the server");
-        start = new JLabel("Waiting for connection...");
-
-        start.setVisible(false);
-
-        label.setBounds(50, 100, 200, 30);
-        start.setBounds(50, 170, 200, 30);
-        btn.setBounds(50, 130, 200, 40);
-
-        btn.addActionListener(new ActionListener() {
+        //Connect button connect function
+        conn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e)
             {
                 new Thread(() -> {
-                    Server server = new Server(5000);
+
+                    Server server = new Server( 5000);
                 }).start();
 
-                start.setVisible(true);
             }
         });
 
-        frame.add(label);
-        frame.add(btn);
-        frame.add(start);
+        // Elements
+        frame.add(title);
+        frame.add(conn);
+        frame.add(status);
+        frame.add(ip);
 
-        frame.setSize(330, 400);
+        // Finalize
+        frame.add(panel);
+        frame.setSize(400, 400);
         frame.setLayout(null);
         frame.setVisible(true);
 
+        // Exit code on window close
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent we) {
                 System.exit(0);
